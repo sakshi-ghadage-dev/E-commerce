b704867e5414b006247f837f3f1813d810ef7561
sakshighadage324-spec <sakshighadage324@gmail.com>
2026-02-22 11:48:41 +0530

    Updated

--- newone/src/step1/Calculator.jsx
+++ newone/src/step1/Calculator.jsx
@@ -1,8 +1,12 @@
-
+import App from '/Add';
+import Subttraction from '/Subtraction';
+import Multiplication from '/Multiplication';
+import Division from '/Division';
 
 let Calculator=()=>{
   return(
     <>
+    <h1>Calculator App</h1>
     <Add/>
     <Subttraction/>
     <Multiplication/>
