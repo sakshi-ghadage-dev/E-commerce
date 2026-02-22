d20335d2884d879a10f2e5c5036db67939c3d7ed
Roshu15 <chavanroshani64@gmail.com>
2026-02-22 11:58:41 +0530

    added multipliacation and division

---
+++ newone/src/step1/Multiplication.jsx
@@ -0,0 +1,12 @@
+let Multiplication = () => {
+
+    const a = 10;
+    const b = 20;
+    return (
+        <div>
+            this is Multiplication {a + b}
+        </div>
+    )
+}
+
+export default Multiplication;
---
+++ newone/src/step1/division.jsx
@@ -0,0 +1,12 @@
+let Division = () => {
+
+    const a = 20;
+    const b = 10;
+    return (
+        <div>
+            this is Division {a / b}
+        </div>
+    )
+}
+
+export default Division;
