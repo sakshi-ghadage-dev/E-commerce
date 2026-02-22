ca7413699b17518b88b6da0e09ded261cf01bb25
sakshighadage324-spec <sakshighadage324@gmail.com>
2026-02-22 12:06:35 +0530

    Updated

--- newone/src/step1/Calculator.jsx
+++ newone/src/step1/Calculator.jsx
@@ -8,13 +8,24 @@ let Calculator=()=>{
   const num1=20;
   const num2=10;
   return(
-    <div>
-    <h1>Calculator App</h1>
-    <Add a={num1} b={num2}/>
-    <Subttraction a={num1} b={num2}/>
-    <Multiplication a={num1} b={num2}/>
-    <Division a={num1} b={num2}/> 
-    </div>
+    <>
+    <div className="flex justify-center">
+                <div className="border-3 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
+                    <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md">Calculator App</h1>
+                    <div className="flex">
+                        <input type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num1" />
+                        <input type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num2" />
+                    </div>
+                </div>
+            </div>
+      
+      <h1>Calculator App</h1>
+      <Add a={num1} b={num2}/>
+      <Subttraction a={num1} b={num2}/>
+      <Multiplication a={num1} b={num2}/>
+      <Division a={num1} b={num2}/> 
+  
+    </>
   )
 }
 
